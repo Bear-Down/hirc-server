73204d56acc3555e2e8f6d5b20881064010c0665
Kevin <kevin.dacanay27@gmail.com>
2026-03-26 11:33:11 -0500

    Kevin Dacanay - Minor Fix server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, "index.html"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
